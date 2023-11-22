addd7fbeff944091632762506f4aaf7464d169a4
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-11-22 10:06:15 +0600

    first commit

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
   git branch -M main
   git push -u origin main
    -->
+
+<!-- https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous -->
+
+//https://www.jsv9000.app/
---
+++ asyncronous.js
@@ -0,0 +1,42 @@
+// basic concept of asyncronous programming
+console.log(1) //Syncronous program to run first
+
+setTimeout(()=>{
+    console.log (2)  /** all the programs will be on a queue */
+}, 0)
+setTimeout(()=>{
+    console.log (3)
+}, 0)
+setTimeout(()=>{
+    console.log (4)
+}, 0)
+setTimeout(()=>{
+    console.log (5)
+}, 0)
+setTimeout(()=>{
+    console.log (6)
+}, 0)
+
+console.log(7); //Syncronous program to run second
+// after all the syncronous programs Asyncronous programs will run
+
+// lets discuss it in brief
+function main(){
+    setTimeout(()=>{
+        console.log('Asyncronous Program Last');
+    }, 10)
+
+    setTimeout(()=>{
+        console.log('Asyncronous Program Fast');
+    }, 0)
+
+    test();
+}
+
+function test(){
+    console.log('Syncronous Program last')
+}
+
+
+console.log ('Syncronous Program First');
+main();
